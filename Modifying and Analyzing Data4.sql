3090fa3a379c0e01c104bbfa306c359fe3c51e1a
Huanchen Lu <luhuanch@umich.edu>
2018-08-23 22:57:00 -0700

    Update

--- Modifying and Analyzing Data4.sql
+++ Modifying and Analyzing Data4.sql
@@ -54,47 +54,4 @@ DATE(('now')- Birthday) AS Age
 
 
 
--- Case Statement
-SELECT 
-employeeid
-,firstname
-,lastname
-,city
-,CASE city
-	WHEN 'Calgary' THEN 'Calgary'
-ELSE 'Other'
-	END
-FROM Employees
-ORDER BY LastName, FirstName;
-
--- 第一种用法
--- case sex
---   when '1' then '男'
---   when '2' then '女’
---   else '其他' END
-
--- 第二种用法
--- case when sex = '1' then '男'
---      when sex = '2' then '女'
---      else '其他' end  
-
-
--- Views(创建视表)
-视图包含行和列，就像一个真实的表。视图中的字段就是来自一个或多个数据库中的真实的表中的字段。
-我们可以向视图添加 SQL 函数、WHERE 以及 JOIN 语句，我们也可以提交数据，就像这些来自于某个单一的表。
-注释：数据库的设计和结构不会受到视图中的函数、where 或 join 语句的影响。
-
-Create View my_view
-AS
-SELECT
-r.regoin
-,t.territory
-,e.reportsto
-FROM Region r
-INNER JOIN Territories t on r.region = t.region
-
-SELECT*FROM my_view (可以看你的view)
-Drop View my_view (删除你创建的view)
-
-
-/* Data Governance and Profiling
+/* Case Statement
